aa1c84c421913723e6e7b139009422bcb20f775e
Nhat Nguyen <nhatng24@umd.edu>
2025-10-03 20:08:06 -0400

    added config

---
+++ flask_app/config.py
@@ -0,0 +1,9 @@
+# Stores all configuration values
+import certifi
+import os
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MONGODB_HOST = os.environ.get('MONGODB_HOST')
+GOOGLE_FORM_LINK = os.environ.get('GOOGLE_FORM_LINK')
+
